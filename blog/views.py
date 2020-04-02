c938cd9dc9a44dad7d27a3fda7f80c4dbb187d0a
Debashis <dmohanty962@gmail.com>
2020-04-02 22:30:58 +0530

    blog_2nd_stage

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,7 @@ from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-    blogs=Blog.objects.all()
+    #blogs=Blog.objects.all()
+    #if we want to order by date:
+    blogs=Blog.objects.order_by('-Date')[:5]
     return render(request,'blog/all_blogs.html',{'BLOGS':blogs})
