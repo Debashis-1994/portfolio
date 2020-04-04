66101722c253a9edd97389facf79313fbdc52a0d
Debashis <dmohanty962@gmail.com>
2020-04-04 19:27:22 +0530

    blog_3rd_stage

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django',
+        'USER': 'postgres',
+        'PASSWORD': 'Gutu@1994',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
